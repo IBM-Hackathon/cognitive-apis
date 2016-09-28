d2d39b3637bcaf93143df43e813df4b59480ba39
Michael Wellner <michael.wellner@de.ibm.com>
2016-09-28 20:49:21 +0200

    cleanup.

--- src/core/withIntl.js
+++
@@ -1,8 +0,0 @@
-import _ from 'lodash';
-import { injectIntl } from 'react-intl';
-
-export default (component) => {
-  const wrapper = injectIntl(component);
-  wrapper.propTypes = _.assign({}, wrapper.propTypes || {}, component.propTypes || {})
-  return wrapper;
-}
--- src/core/withStyles.js
+++
@@ -1,8 +0,0 @@
-import _ from 'lodash';
-import withStyles from 'isomorphic-style-loader/lib/withStyles';
-
-export default (style) => (component) => {
-    const wrapper = withStyles(style)(component);
-    wrapper.propTypes = _.assign({}, wrapper.propTypes || {}, component.propTypes || {});
-    return wrapper;
-};
